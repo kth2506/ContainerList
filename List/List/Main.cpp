126e8d30f39e83e582904388202fa3ceec710faf
kim taehwan <ksj4159@naver.com>
2022-06-13 00:59:18 +0900

    // ** List ver.02 06.12

--- List/List/Main.cpp
+++ List/List/Main.cpp
@@ -1,4 +1,4 @@
-// ** List ver.01 06.10
+// ** List ver.02 06.12
 
 #include <iostream>
 
@@ -15,7 +15,7 @@ void AddObject(List* _Next, const int& _value);
 
 void Output(List* _Next);
 
-void Insert(List* _Next, const int& _where);
+void Insert(List* _Next, const int& _where, const int& _value);
 
 void Erase(List* _Next, const int& _where);
 
@@ -29,6 +29,8 @@ int main(void)
 		AddObject(NumberList, i * 10 + 10);
 	}
 
+	//Insert(NumberList, 5, 2);
+	Erase(NumberList, 2);
 	Output(NumberList);
 	return 0;
 }
@@ -58,10 +60,25 @@ void Output(List* _Next)
 
 void Insert(List* _Next, const int& _where, const int& _value)
 {
-	AddObject(_Next, _value);
+	if (_where > 1)
+		Insert(_Next->Back, _where - 1, _value);
+	else
+		_Next->Value = _value;
 }
 
 void Erase(List* _Next, const int& _where)
 {
+	if(_where > 1)
+		Erase(_Next->Back, _where - 1);
+	else if (_Next->Back == nullptr)
+	{
+		_Next = nullptr;
+	}
+	else 
+	{
+		_Next->Value = _Next->Back->Value;
+		Erase(_Next->Back, 0);
+		
+	}
 
 }
